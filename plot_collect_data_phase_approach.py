3c712813a5990e4e0c60a9dd0cff707d38f48d49
Embodied-AI-Team <y10240071@mail.ecust.edu.cn>
2025-06-22 13:24:22 +0800

    modify plot result function

--- plot_collect_data_phase_approach.py
+++ plot_collect_data_phase_approach.py
@@ -16,8 +16,8 @@ def plot_collect_data_trajectory(trajectories,  colors, labels):
     for idx, data in enumerate(trajectories):
         # 绘制轨迹线
         ax.plot(
-            data[:len(data)-5, 0], data[:len(data)-5, 1], data[:len(data)-5, 2],
-            f'{colors[idx]}-', label=labels[idx], linewidth=1.5, alpha=0.6
+            data[:len(data)-5, 0], data[:len(data)-5, 1], data[:len(data)-5, 2], linestyle='-',
+            color=colors[idx], label=labels[idx], linewidth=2
         )
 
         # 在每个点绘制局部坐标系（调整step控制密度）
@@ -34,6 +34,7 @@ def plot_collect_data_trajectory(trajectories,  colors, labels):
             print(np.linalg.norm(quat))
             print(np.allclose(rot_matrix.T @ rot_matrix, np.eye(3), atol=1e-6))
             # 绘制X/Y/Z轴（RGB颜色）
+            c = 1
             for axis, color in zip(rot_matrix.T, ['g', 'r', 'b']):
                 ax.quiver(
                     pos[0], pos[1], pos[2],
@@ -42,18 +43,27 @@ def plot_collect_data_trajectory(trajectories,  colors, labels):
                     arrow_length_ratio=0.1,
                     linewidth=0.5,
                     alpha=0.7,  # 半透明避免遮挡
-                    label=f'{labels[idx]} Frame' if (idx == 0 and i == 0 and axis[0] > 0) else ""
+                    label=f'{labels[c]}' if (idx == 0 and i == 0) else ""
                 )
+                c += 1
 
     # 设置图形属性
     # ax.set_xlim([-0.1, 0.5])
     # ax.set_ylim([-0.1, 0.5])
     # ax.set_zlim([0.429999, 0.4301])
-    ax.set_xlabel('X (m)')
-    ax.set_ylabel('Y (m)')
-    ax.set_zlabel('Z (m)')
+
+    ax.set_xlabel('X (m)', labelpad=15, fontsize=14)
+    ax.set_ylabel('Y (m)', labelpad=15, fontsize=14)
+    ax.set_zlabel('Z (m)', labelpad=15, fontsize=14)
+    ax.view_init(elev=30, azim=45)  # 固定视角
+    ax.tick_params(axis='x', pad=10, labelsize=12)
+    ax.tick_params(axis='y', pad=10, labelsize=12)
+    ax.tick_params(axis='z', pad=10, labelsize=12)
+    ax.xaxis.pane.set_facecolor('none')
+    ax.yaxis.pane.set_facecolor('none')
+    ax.zaxis.pane.set_facecolor('none')
     # ax.set_title('Robot 3D Trajectory with Euler Angle Frames')
-    ax.legend()
+    ax.legend(fontsize=14)
     plt.tight_layout()
     plt.show()
 
@@ -111,8 +121,8 @@ if __name__ == '__main__':
     trajectories = [np.load(f) for f in file_names]  # 如果是.csv，用 np.loadtxt(f, delimiter=',')
 
     # 为每组轨迹设置颜色和标签
-    colors = ['y', 'm']  # 黄色、洋红色
-    labels = ['Robot A', 'Robot B']
+    colors = ['black', 'm']  # 黄色、洋红色More actions
+    labels = ['trajectories', 'roll', 'pitch', 'yaw']
 
 
     plot_collect_data_trajectory(trajectories, colors, labels)
