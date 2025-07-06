01d1aa7f26f97204bee27e4c6c9f684846aa9303
Embodied-AI-Team <y10240071@mail.ecust.edu.cn>
2025-07-06 09:31:13 +0800

    modify phase insertion to support plot assembly

--- eval_for_plot_phase_insertion.py
+++ eval_for_plot_phase_insertion.py
@@ -46,6 +46,8 @@ def main(cfg: OmegaConf):
 
     obs = env.reset()
     # 初始化参数
+    # action_eval = np.zeros((30, 7))
+    action_eval = np.empty((0, 3))
     writer = SummaryWriter('./HDQN_peg/experimental_result_insertion')
     angles = []
     angles_select = [-150, -30, 90, 210]
@@ -146,6 +148,9 @@ def main(cfg: OmegaConf):
             time.sleep(time_until_next_step)
         robot_state_position = p.getLinkState(env.ur5_id, env.ur5EndEffectorIndex, computeForwardKinematics=1)
         curr_pose = robot_state_position[0]
+        action_pose = np.array(curr_pose)
+        # action_eval[step_num, 0:3] = curr_pose
+        action_eval = np.vstack((action_eval, action_pose))
         """ draw contact phase result """
         cur_peg_pos = p.getLinkState(env.ur5_id, 8)[4]
         cur_peg_orie = p.getLinkState(env.ur5_id, 8)[5]
@@ -165,6 +170,7 @@ def main(cfg: OmegaConf):
         """ draw contact phase result """
         if curr_pose[2] < 0.40 and np.linalg.norm(Fext) < 1:
             done = True
+            np.save("action_HDQN_eval_circle_insertion.npy", action_eval)
     p.disconnect()
 
 
