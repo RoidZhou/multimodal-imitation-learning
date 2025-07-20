e699077605f05d68ab80daceefc1391f417afa1f
Embodied-AI-Team <y10240071@mail.ecust.edu.cn>
2025-07-20 15:50:06 +0800

    plot diffusion result for circle and square peg-hole onn phase insertion

---
+++ eval_circle_for_plot_phase_insertion.py
@@ -0,0 +1,169 @@
+import sys
+
+import pylab as p
+
+sys.path.append('./imitation_learning_idp3')
+
+import os
+import dill
+import torch
+import time
+import numpy as np
+import open3d as o3d
+import pathlib
+from omegaconf import OmegaConf
+import hydra
+import pybullet as p
+from scipy.spatial.transform import Rotation as R
+from numpy.ma.core import argmin
+from tensorboardX import SummaryWriter
+sys.stdout = open(sys.stdout.fileno(), mode='w', buffering=1)
+sys.stderr = open(sys.stderr.fileno(), mode='w', buffering=1)
+
+os.environ['WANDB_SILENT'] = "True"
+
+OmegaConf.register_new_resolver("eval", eval, replace=True)
+
+
+@hydra.main(
+    version_base=None,
+    config_path=str(pathlib.Path(__file__).parent.joinpath(
+        'imitation_learning_idp3', 'config'))
+)
+def main(cfg: OmegaConf):
+    OmegaConf.resolve(cfg)
+    policy = hydra.utils.instantiate(cfg.policy)
+    env = hydra.utils.instantiate(cfg.task.env)
+    device = 'cuda:0'
+    device = torch.device(device)
+    policy.load_state_dict(torch.load("model_600_phase_insertion.pth", map_location=device, pickle_module=dill))
+    policy.to(device)
+    policy.eval()
+
+    n_obs_steps = cfg.n_obs_steps
+    n_action_steps = cfg.n_action_steps
+    horizon = cfg.horizon
+
+    obs = env.reset()
+    # 初始化参数
+    action_eval = np.empty((0, 7))
+    writer = SummaryWriter('./HDQN_peg/experimental_result_insertion/HDPRL')
+    angles = []
+    angles_select = [-150, -30, 90, 210]
+    for i in range(4):
+        hole_orientation = R.from_euler('xyz', [90, 90, -150 + 120 * i], degrees=True).as_quat()  # 默认固定孔的姿态
+
+        robot_state_position = p.getLinkState(env.ur5_id, env.ur5EndEffectorIndex, computeForwardKinematics=1)
+        state_orientation = robot_state_position[1]
+        orientation_err = np.dot(hole_orientation, state_orientation)
+        orien_angle_err = 2 * np.arccos(orientation_err)
+        angle_err = orien_angle_err * 180 / np.pi
+        angles.append(angle_err)
+    angle_target = angles_select[argmin(angles)]
+    hole_orientation = R.from_euler('xyz', [90, 90, angle_target], degrees=True).as_quat()  # 默认固定孔的姿态
+
+    desired_r = np.array(hole_orientation)
+
+    # desired_r = [-0.61237244, 0.35355339, 0.61237244, 0.35355339]
+    hole_pose = np.array(env.inint_pose_print)
+    # 移动到初始位姿
+    env.go(list(hole_pose), desired_r)
+    FT = env.getForceTorque()
+    Fext = [FT[0], FT[1], FT[2]]
+    print("force : ", Fext)
+    # 移动到初始插接位姿
+    hole_pose[2] = hole_pose[2] - 0.005  # 孔深度0.08
+    env.go(hole_pose, desired_r)
+
+    init_x_angle_err= np.random.uniform(-1, 1)
+    init_y_angle_err= np.random.uniform(-1, 1)
+
+    hole_orientation = R.from_euler('xyz', [90+init_x_angle_err, 90+init_y_angle_err, angle_target], degrees=True).as_quat()  # 默认固定孔的姿态
+    hole_orie = np.array(hole_orientation)
+    # 移动到初始位姿
+    env.go(hole_pose, hole_orie)
+
+    FT = env.getForceTorque()
+    Fext = [FT[0], FT[1], FT[2]]
+    print("force : ", Fext)
+    # point_cloud = np.zeros((1, horizon, *obs['point_cloud'].shape))
+    agent_pos = np.zeros((1, horizon, *obs['agent_pos'].shape))
+    for i in range(horizon):
+        agent_pos[0, i, ...] = obs['agent_pos']
+    observation = {}
+    observation['agent_pos'] = agent_pos
+
+    done = False
+    step_num = 0
+
+    while not done:
+        step_start = time.time()
+
+        if step_num % n_action_steps == 0:
+            observation['agent_pos'][0, 0:n_obs_steps - 1, ...] = observation['agent_pos'][0, 1:n_obs_steps, ...]
+
+            observation['agent_pos'][0, n_obs_steps - 1, ...] = obs['agent_pos']
+
+            """ visualize point cloud """
+            # point_cloud = obs['point_cloud']
+            # sampled_points = env.uniform_sampling(point_cloud)
+            # pcd = o3d.geometry.PointCloud()
+            # pcd.points = o3d.utility.Vector3dVector(sampled_points[:, :3])
+            # pcd.colors = o3d.utility.Vector3dVector(sampled_points[:, 3:] / 255.0)
+            # o3d.visualization.draw_geometries([pcd])
+            """ visualize point cloud """
+
+            actions = policy.predict_action(observation)['action'].cpu().detach().numpy()
+
+        action = actions[0, step_num % n_action_steps, :]
+        obs, reward, done, info = env.step(action)
+
+        writer.add_scalars("force_x",
+                                {"force_x": env.force_x}, step_num)
+        writer.add_scalars("force_y",
+                                {"force_y": env.force_y}, step_num)
+        writer.add_scalars("force_z",
+                                {"force_z": env.force_z}, step_num)
+        writer.add_scalars("Torque_x",
+                                {"Torque_x": env.Torque_x}, step_num)
+        writer.add_scalars("Torque_y",
+                                {"Torque_y": env.Torque_y}, step_num)
+        writer.add_scalars("Torque_z",
+                                {"Torque_z": env.Torque_z}, step_num)
+
+        step_num += 1
+        time_until_next_step = 1/env._timeStep - (time.time() - step_start)
+        if time_until_next_step > 0:
+            time.sleep(time_until_next_step)
+        robot_state_position = p.getLinkState(env.ur5_id, env.ur5EndEffectorIndex, computeForwardKinematics=1)
+        curr_pose = robot_state_position[0]
+        action_pose = np.zeros(7)
+        action_pose[:3] = curr_pose
+        action_eval = np.vstack((action_eval, action_pose))
+        """ draw contact phase result """
+        cur_peg_pos = p.getLinkState(env.ur5_id, 8)[4]
+        cur_peg_orie = p.getLinkState(env.ur5_id, 8)[5]
+        cur_peg_pos = np.array(cur_peg_pos)
+        cur_peg_pos[2] -= 0.025
+        if env.prev_pos is not None:
+            # 绘制线段连接当前位置和上一个位置
+            p.addUserDebugLine(env.prev_pos, cur_peg_pos,
+                               lineColorRGB=[0.0, 1.0, 1.0],  # 红色
+                               lineWidth=4,
+                               lifeTime=env.trail_duration)
+        else:
+            env.prev_pos = cur_peg_pos
+
+        env.prev_pos = cur_peg_pos
+        # 关键帧绘制坐标系
+        if step_num % 2 == 0:
+            env.draw_coordinate_frame(cur_peg_pos, cur_peg_orie, axis_length=0.01, lifetime=0)
+        """ draw contact phase result """
+        if curr_pose[2] < 0.40 and np.linalg.norm(Fext) < 1:
+            done = True
+            np.save("action_HDQN_eval_circle_insertion.npy", action_eval)
+    p.disconnect()
+
+
+if __name__ == '__main__':
+    main()
